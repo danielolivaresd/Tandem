a60d79b12f0dc3f6d23751df0b34e0e600c7281e
Daniel Olivares <danieloddl2@hotmail.com>
2015-04-09 13:57:46 -0500

    Seed Languages for user. Show them on their 'edit profile' view.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Language.destroy_all
+Language.create!([
+	{
+		name:"English"
+	},
+	{
+		name:"Spanish"
+	},
+	{
+		name:"German"
+	},
+	{
+		name:"French"
+	}
+])
+english = Language.find_by(name:"English")
+spanish = Language.find_by(name:"Spanish")
+german = Language.find_by(name:"German")
+french = Language.find_by(name:"French")
+
+UserLanguage.destroy_all
+UserLanguage.create!([
+	{
+		user: User.first,
+		language: english,
+		level: 10,
+		is_native: true
+	},
+	{
+		user: User.first,
+		language: spanish,
+		level: 3,
+		is_native: false
+	},
+])
+
+p "#{Language.all.count} languages created."
+p "#{UserLanguage.all.count} user languages created."
