9e4a7648ed0bbec5d32465da1c056dea0ae70629
Daniel Olivares <danieloddl2@hotmail.com>
2015-04-25 13:22:26 -0500

    Fix issue 'not going to home'. and links in profile.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,5 @@
 class StaticController < ApplicationController
 	before_action :set_user
-	before_action :authenticate_user!
 	before_action :set_all_languages
 
 	def index
@@ -18,21 +17,5 @@ private
 
 	def set_all_languages
 		@all_languages = Language.all
-	end
-
-	def get_match(match_user)
-		match_user.match
-	end
-
-	def get_other_user(match)
-		match.match_users.where(match: match).not(user: @user).first.user
-	end
-
-	def get_teacher_language(match)
-		match.match_language.where(match: match, teacher: @user).language
-	end
-
-	def get_student_language(match)
-		match.match_language.where(match: match, student: @user).language
 	end
 end
